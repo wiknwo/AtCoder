8af5e9cde2ad5a640db079a5a3848bea3af78578
wiknwo <spinningthewww@outlook.com>
2021-03-04 10:39:36 +0000

    Accepted

---
+++ welcome_to_atcoder.py
@@ -0,0 +1,15 @@
+'''Welcome to Atcoder
+
+    William Ikenna-Nwosu (wiknwo)
+'''
+def main():
+    data = []
+    a = int(input(""))
+    data = input("").split(" ")
+    b = int(data[0])
+    c = int(data[1])
+    s = input("")
+
+    print("{} {}".format(a + b + c, s))
+if __name__ == '__main__':
+    main()
